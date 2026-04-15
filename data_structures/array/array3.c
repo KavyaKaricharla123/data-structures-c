f459e261f5197ed6851a461a7eeb61d42d72f571
KavyaKaricharla123 <kavyakaricharla@gmail.com>
2026-04-15 14:54:27 +0530

    reverse array

---
+++ data_structures/array/array3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//reverse array
+
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=5;
+    int left=0;
+    int right=n-1;
+    while(left<right){
+        int temp=arr[left];
+        arr[left]=arr[right];
+        arr[right]=temp;
+
+    left++;
+    right--;
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+    
+
+
+}
