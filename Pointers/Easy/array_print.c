4ed8142fc2d77ae306ca5886ea2390a34615347e
KavyaKaricharla123 <kavyakaricharla@gmail.com>
2026-05-18 16:23:34 +0530

    array_elements

---
+++ Pointers/Easy/array_print.c
@@ -0,0 +1,18 @@
+// printing array elements
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[5] = {1,2,3,4,5};
+    int *p = arr;
+    for(int i=0;i<5;i++){
+        printf("%d\n",*p);
+        p++;
+
+    }
+    return 0;
+
+
+}
+
